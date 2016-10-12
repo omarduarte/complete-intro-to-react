41e2e81578d7865dba33846b379f24dea17980cc
Omar Duarte <omarduarte@gmail.com>
2016-10-12 20:29:46 +0300

    users MyTitle instance instead of h1

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,19 @@
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
+var MyTitle = React.createClass({
+	render() {
+		return (
+			div(null, 
+				h1(null, 'Check out this other thing')
+			)
+		)
+	}
+})
+
 var MyFirstcomponent = (
 	div(null,
-		h1(null, 'This is a component')
+		React.createElement(MyTitle, null)
 	)
 )
 
